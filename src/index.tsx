3c9b3508187b4d3f42ec15deecd70f7f217ffebe
denisSorokinz <d.sorokin@gmail.com>
2020-10-07 01:19:18 +0300

    fixes

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
 import "./styles/reset.css";
+
+// ! - we are sure that the element exists
+const root = document.querySelector("#root")!;
+
+ReactDOM.render(<App />, root);
